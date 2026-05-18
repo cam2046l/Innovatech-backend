ec1b2dc0293a35b864b09cc5fc453e21582caeb1
camilo2046M <ca.menav@duocuc.cl>
2026-05-18 10:35:00 -0400

    Prueba 15

--- back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/SpringbootApiRestApplication.java
+++ back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/SpringbootApiRestApplication.java
@@ -2,7 +2,7 @@ package com.citt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Prueba
 @SpringBootApplication
 public class SpringbootApiRestApplication {
 
